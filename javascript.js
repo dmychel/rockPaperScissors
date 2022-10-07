d2808fbdb06b857b4ee6be1e07ae9e1347c2101a
dmychel <dmychel@tutanota.com>
2022-10-07 15:20:03 -0400

    created a couople of functions: The function winner(playerSelection,computerSelection) defines what beats what (rock>scissors, paper>rock etc) and has a return 'player' else 'computer. The function round(playerSelection,computerSelection) uses the winner function to display a winning/losing/tie message in the console

--- javascript.js
+++ javascript.js
@@ -1,1 +1,53 @@
+//rock, paper, scissors ^
+//computer needs to select an option(rock,paper,scissors)randomly ^
+//rock needs to beat scissors
+//paper needs to beat rock
+//scissors needs to beat paper
+//same choice === tie
+
+
 console.log('Hello World');
+
+const options = ['rock', 'paper', 'scissors']
+
+function getComputerChoice(){
+    const compChoice = options[Math.floor(Math.random() * options.length)];
+    console.log(compChoice);
+    return compChoice;
+   
+}
+
+function winner(playerSelection,computerSelection){
+    if(playerSelection === computerSelection){
+        return 'tie';
+    }
+    if(
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper') ||
+        (playerSelection === 'paper' && computerSelection === 'rock')
+    ){
+        return 'player';
+    }
+    else{
+        return 'computer';
+    }
+}
+
+function round(playerSelection,computerSelection){
+    const result = winner(playerSelection,computerSelection);
+    if(result === 'tie'){
+        return 'Wow! it \'s a tie!';
+    } 
+    if(result === 'player'){
+        return `Wow! you win! ${playerSelection} beats ${computerSelection}`;
+    }
+    else if(result === 'computer'){
+        return `Oh no! you lost, ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+
+const playerSelection = 'rock'
+const computerSelection = getComputerChoice();
+console.log(playerSelection);
+console.log(round(playerSelection,computerSelection));
