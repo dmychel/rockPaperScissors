e67b3f41fd8573e8bdec43e297a35d15fb464244
dmychel <dmychel@tutanota.com>
2022-11-15 10:57:46 -0500

    I have edited some of the CSS. I also redid some of the game logic and cached each HTML element that will be needed to display the results on the webpage. I will be working on displaying the results next.

--- javascript.js
+++ javascript.js
@@ -1,103 +1,68 @@
-console.log('Hello! Welcome to the battle! Choose your weapon carefully!');
+//run function getPlayerChoice.
+//run function getComputerChoice. done
+//run function checkWinner done
+//run game in a loop 5 times
+//compare scores, display winner
+const options = ['knight', 'cavalry', 'archer'];
 
-//options
-const options = ['archer', 'horseback', 'knight'];
+const playerChoice_h2 = document.getElementById('playerChoice');
+const computerChoice_h2 = document.getElementById('computerChoice');
+const playerScore_h2 = document.getElementById('playerScore');
+const computerScore_h2 = document.getElementById('computerScore');
+const result_h2 = document.getElementById('result');
+let playerScore = 0;
+let computerScore = 0;
 
-//random compChoice
-function getCompChoice(compSelection){
-   compSelection = options[Math.floor(Math.random() * 3)];
-   console.log(compSelection);
-   return compSelection
-}
-
-
-
-
-//player choice
 function getPlayerChoice(playerSelection){
-    let validOption = false;
-    while(validOption == false){
-        playerSelection = prompt('Choose your warrior! Knight, Archer, or Horseback').toLowerCase();
-        if(playerSelection == null){
-            continue;
-        }
-        if(options.includes(playerSelection)){
-            validOption == true;
-            return playerSelection;  
-        }
-    }
-}
+    playerSelection = 'archer'
+    return playerSelection
+};
 
+function getComputerChoice(computerSelection){
+    computerSelection = options[Math.floor(Math.random() * 3)];
+    return computerSelection;
+};
 
-function playRound(playerSelection, compSelection){
+function checkWinner(playerSelection,computerSelection){
     if(
-         (playerSelection === 'archer' && compSelection === 'knight') ||
-         (playerSelection === 'horseback' && compSelection === 'archer') ||
-         (playerSelection === 'knight' && compSelection === 'horseback')
-     ){
-         return `You win! ${playerSelection} beats ${compSelection}!`
-     }
-     else if(
-         (compSelection === 'archer' && playerSelection === 'knight') ||
-         (compSelection === 'horseback' && playerSelection === 'archer') ||
-         (compSelection === 'knight' && playerSelection === 'horseback')
-     ){
-         return `You lose! ${compSelection} beats ${playerSelection}!`
-     }
-     else{
-         return `It's  tie!`
-     }
- }
-
-
- 
-
-//round logic
-function checkWinner(playerSelection, compSelection){
-    console.log('Choose your weapon next weapon!')
-   if(
-        (playerSelection === 'rock' && compSelection === 'scissor') ||
-        (playerSelection === 'paper' && compSelection === 'rock') ||
-        (playerSelection === 'scissor' && compSelection === 'paper')
-    ){
-        return 'player'
-    }
+        (playerSelection === 'archer' && computerSelection === 'knight') ||
+        (playerSelection === 'knight' && computerSelection === 'cavalry') ||
+        (playerSelection === 'cavlary' && computerSelection === 'archer')){
+            return 'player'
+        }
     else if(
-        (compSelection === 'rock' && playerSelection === 'scissor') ||
-        (compSelection === 'paper' && playerSelection === 'rock') ||
-        (compSelection === 'scissor' && playerSelection === 'paper')
-    ){
-        return 'computer'
-    }
-    else{
-        return 'tie'
-    }
-}
-
-
-//game logic
-function gameLogic(playerSelection, compSelection){
-    let playerScore = 0;
-    let compScore = 0;
-    for(let i = 0; i < 5; i++){
-        const playerSelection = getPlayerChoice();
-        const compSelection = getCompChoice();
-        console.log('Choose your next warrior!')
-        console.log(playRound(playerSelection,compSelection));
-        console.log('--------------------------------------');
-        if(checkWinner(playerSelection,compSelection) == 'player'){
-            playerScore++;
+        (computerSelection === 'archer' && playerSelection === 'knight') ||
+        (computerSelection === 'knight' && playerSelection === 'cavalry') ||
+        (computerSelection === 'cavalry' && playerSelection === 'archer')){
+            return 'computer'
         }
         else{
-            compScore++
+            return 'tie'
+        };
+};
+
+function gameLogic(playerSelection,computerSelection){
+    for(let i = 0; i < 5; i++){
+        let playerSelection = getPlayerChoice();
+        let computerSelection = getComputerChoice();
+
+        if(checkWinner(playerSelection,computerSelection) == 'player'){
+            playerScore++
         }
-    }
-    if(playerScore > compScore){
-        console.log('You Won the War!!!')
-    }
-    else{
-        console.log('You Lost the War!!!')
-    }
-}
+        else if(checkWinner(playerSelection,computerSelection) == 'computer'){
+            computerScore++
+        }
+        else {
+            return null;
+        }
+
+
+        //console.log('playerSelction is ' + playerSelection);
+        //console.log('computerSelection is ' + computerSelection)
+    };
+};
 
 gameLogic()
+
+
+
