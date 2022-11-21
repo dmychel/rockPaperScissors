bd421686794ba352fd316deb2b762c392eeffbd7
dmychel <dmychel@tutanota.com>
2022-11-21 18:13:33 -0500

    I have finisnhed 90% of the project. The player chooses their selection by clicking one of three buttons, the input triggers a random computer choice that is displayed on the webpage and compared. The game keeps track of the computerScore and playerScore. The first player to reach 5 wins, wins the game and then the game stops.

--- javascript.js
+++ javascript.js
@@ -5,64 +5,112 @@
 //compare scores, display winner
 const options = ['knight', 'cavalry', 'archer'];
 
-const playerChoice_h2 = document.getElementById('playerChoice');
-const computerChoice_h2 = document.getElementById('computerChoice');
-const playerScore_h2 = document.getElementById('playerScore');
-const computerScore_h2 = document.getElementById('computerScore');
+let playerChoice_h2 = document.getElementById('playerChoice');
+let computerChoice_h2 = document.getElementById('computerChoice');
+let playerScore_h2 = document.getElementById('playerScore');
+let computerScore_h2 = document.getElementById('computerScore');
 const result_h2 = document.getElementById('result');
+const cavalry = document.getElementById('cavalry');
+const archer = document.getElementById('archer');
+const knight = document.getElementById('knight');
+const endGame_h2 = document.getElementById('endGame');
+let wrapper_h2 = document.getElementById('wrapper')
+let resultContainer_div = document.getElementById('result')
+
 let playerScore = 0;
-let computerScore = 0;
+let computerScore = 0
 
-function getPlayerChoice(playerSelection){
-    playerSelection = 'archer'
-    return playerSelection
-};
-
-function getComputerChoice(computerSelection){
-    computerSelection = options[Math.floor(Math.random() * 3)];
-    return computerSelection;
-};
-
-function checkWinner(playerSelection,computerSelection){
-    if(
-        (playerSelection === 'archer' && computerSelection === 'knight') ||
-        (playerSelection === 'knight' && computerSelection === 'cavalry') ||
-        (playerSelection === 'cavlary' && computerSelection === 'archer')){
-            return 'player'
-        }
-    else if(
-        (computerSelection === 'archer' && playerSelection === 'knight') ||
-        (computerSelection === 'knight' && playerSelection === 'cavalry') ||
-        (computerSelection === 'cavalry' && playerSelection === 'archer')){
-            return 'computer'
-        }
-        else{
-            return 'tie'
-        };
-};
-
-function gameLogic(playerSelection,computerSelection){
-    for(let i = 0; i < 5; i++){
-        let playerSelection = getPlayerChoice();
+    knight.addEventListener('click', function(){
+        let playerSelection = 'knight';
+        let computerSelection = getComputerChoice();
+        
+        console.log(playerSelection);
+        console.log(computerSelection);
+        playRound(playerSelection,computerSelection);
+        checkScore(playerSelection,computerSelection);
+        resetGame(playerScore,computerScore);
+        playerChoice_h2.innerHTML = playerSelection;
+        computerChoice_h2.innerHTML = computerSelection;
+    });
+    
+    archer.addEventListener('click', function(){
+        let playerSelection = 'archer';
         let computerSelection = getComputerChoice();
 
-        if(checkWinner(playerSelection,computerSelection) == 'player'){
-            playerScore++
-        }
-        else if(checkWinner(playerSelection,computerSelection) == 'computer'){
-            computerScore++
-        }
-        else {
-            return null;
-        }
+        console.log(playerSelection);
+        console.log(computerSelection);
+        playRound(playerSelection,computerSelection);
+        checkScore(playerSelection,computerSelection);
+        resetGame(playerScore,computerScore);
+        playerChoice_h2.innerHTML = playerSelection;
+        computerChoice_h2.innerHTML = computerSelection;
+    });
+    
+    cavalry.addEventListener('click', function(){
+        let playerSelection = 'cavalry';
+        let computerSelection = getComputerChoice();
 
-
-        //console.log('playerSelction is ' + playerSelection);
-        //console.log('computerSelection is ' + computerSelection)
+        console.log(playerSelection);
+        console.log(computerSelection);
+        playRound(playerSelection,computerSelection);
+        checkScore(playerSelection,computerSelection);
+        resetGame(playerScore,computerScore);
+        playerChoice_h2.innerHTML = playerSelection;
+        computerChoice_h2.innerHTML = computerSelection;
+    });
+    
+    function getComputerChoice(computerSelection){
+         computerSelection = options[Math.floor(Math.random() * 3)]
+         return computerSelection
     };
-};
 
-gameLogic()
+    function playRound(playerSelection,computerSelection){
+        if(
+            (playerSelection === 'archer' && computerSelection === 'knight') ||
+            (playerSelection === 'knight' && computerSelection === 'cavalry') ||
+            (playerSelection === 'cavalry' && computerSelection === 'archer')){
+                result_h2.innerHTML = `Player Wins! ${playerSelection} beats ${computerSelection}!`
+            }
+        else if(
+            (computerSelection === 'archer' && playerSelection === 'knight') ||
+            (computerSelection === 'knight' && playerSelection === 'cavalry') ||
+            (computerSelection === 'cavalry' && playerSelection === 'archer') ){
+                result_h2.innerHTML = `Player Loses! ${computerSelection} beats ${playerSelection}!`
+            }
+            else{
+                result_h2.innerHTML = 'It\'s a tie!'
+            }
+    }
 
+    function checkScore(playerSelection,computerSelection){
+        if(
+            (playerSelection === 'archer' && computerSelection === 'knight') ||
+            (playerSelection === 'knight' && computerSelection === 'cavalry') ||
+            (playerSelection === 'cavalry' && computerSelection === 'archer')){
+                playerScore++
+                playerScore_h2.innerHTML = playerScore
+                console.log(playerScore)
+            }
+        else if(
+            (computerSelection === 'archer' && playerSelection === 'knight') ||
+            (computerSelection === 'knight' && playerSelection === 'cavalry') ||
+            (computerSelection === 'cavalry' && playerSelection === 'archer') ){
+                computerScore++
+                computerScore_h2.innerHTML = computerScore
+                console.log(computerScore)
+            }
+    }
 
-
+    function resetGame(playerScore,computerScore){
+        if(playerScore === 5){
+            endGame_h2.innerHTML = 'Player Wins the War!!';
+            wrapper_h2.remove();
+            resultContainer_div.remove();
+        }
+        else if(computerScore === 5){
+            endGame_h2.innerHTML = 'Computer Wins the War!!'
+            wrapper_h2.remove();
+            resultContainer_div.remove();
+        }
+    };
+  
